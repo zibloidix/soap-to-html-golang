50ab8f94a47dc27782964cdd03258cc0b50d9de7
Aleksey Lomov <zibloidix@gmail.com>
2022-02-22 12:29:31 +1100

    DEV-99 1. Реализован разбор xsd-схемы на простых типах и импортах

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zibloidix/soap-to-html-golang
+
+go 1.17
--- main.go
+++ main.go
@@ -1,9 +1,50 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
 	"os"
 )
+
+type Schema struct {
+	XMLName     xml.Name     `xml:"schema"`
+	Imports     []Import     `xml:"import"`
+	SimpleTypes []SimpleType `xml:"simpleType"`
+}
+
+type SimpleType struct {
+	XMLName     xml.Name `xml:"simpleType"`
+	Name        string   `xml:"name,attr"`
+	Annotation  Annotation
+	Restriction Restriction
+}
+
+type Annotation struct {
+	XMLName       xml.Name      `xml:"annotation"`
+	Documentation Documentation `xml:"documentation"`
+}
+
+type Documentation struct {
+	XMLName xml.Name `xml:"documentation"`
+	Value   string   `xml:",chardata"`
+}
+
+type Restriction struct {
+	XMLName      xml.Name      `xml:"restriction"`
+	Base         string        `xml:"base,attr"`
+	Enumerations []Enumeration `xml:"enumeration"`
+}
+
+type Enumeration struct {
+	XMLName xml.Name `xml:"enumeration"`
+	Value   string   `xml:"value,attr"`
+}
+
+type Import struct {
+	XMLName        xml.Name `xml:"import"`
+	Namespace      string   `xml:"namespace,attr"`
+	SchemaLocation string   `xml:"schemaLocation,attr"`
+}
 
 func main() {
 	fileName := "./wsdl-xsd/hcs-rvuo-types.xsd"
@@ -11,5 +52,10 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(string(data))
+	var schema Schema
+	err = xml.Unmarshal(data, &schema)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(schema)
 }
