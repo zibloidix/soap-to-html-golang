c5ffe334ebce1db123ed538161dec0ceb1d23da5
Aleksey Lomov <zibloidix@gmail.com>
2022-02-24 14:50:15 +1100

    DEV-101 DEV-102 1. Реализована обработка wsdl 2. Типы для unmarshal вынесены в отдельную схему

--- main.go
+++ main.go
@@ -9,141 +9,39 @@ import (
 // Для примера разбора wsdl и xsd
 // https://github.com/fiorix/wsdl2go/blob/master/wsdl/types.go
 
-type Schema struct {
-	XMLName         xml.Name          `xml:"schema"`
-	TargetNamespace string            `xml:"targetNamespace,attr"`
-	Namespaces      map[string]string `xml:"-"`
-	Imports         []ImportSchema    `xml:"import"`
-	Includes        []IncludeSchema   `xml:"include"`
-	SimpleTypes     []SimpleType      `xml:"simpleType"`
-	ComplexTypes    []ComplexType     `xml:"complexType"`
-	Elements        []Element         `xml:"element"`
-}
-
-type SimpleType struct {
-	XMLName         xml.Name    `xml:"simpleType"`
-	Name            string      `xml:"name,attr"`
-	Union           Union       `xml:"union"`
-	Restriction     Restriction `xml:"restriction"`
-	TargetNamespace string
-}
-
-type Union struct {
-	XMLName     xml.Name `xml:"union"`
-	MemberTypes string   `xml:"memberTypes,attr"`
-}
-
-type Restriction struct {
-	XMLName    xml.Name    `xml:"restriction"`
-	Base       string      `xml:"base,attr"`
-	Enum       []Enum      `xml:"enumeration"`
-	Attributes []Attribute `xml:"attribute"`
-}
-
-type Enum struct {
-	XMLName xml.Name `xml:"enumeration"`
-	Value   string   `xml:"value,attr"`
-}
-
-type ComplexType struct {
-	XMLName         xml.Name       `xml:"complexType"`
-	Name            string         `xml:"name,attr"`
-	Abstract        bool           `xml:"abstract,attr"`
-	Doc             string         `xml:"annotation>documentation"`
-	AllElements     []Element      `xml:"all>element"`
-	ComplexContent  ComplexContent `xml:"complexContent"`
-	SimpleContent   SimpleContent  `xml:"simpleContent"`
-	Sequence        Sequence       `xml:"sequence"`
-	Choice          Choice         `xml:"choice"`
-	Attributes      []Attribute    `xml:"attribute"`
-	TargetNamespace string
-}
-
-type SimpleContent struct {
-	XMLName     xml.Name    `xml:"simpleContent"`
-	Extension   Extension   `xml:"extension"`
-	Restriction Restriction `xml:"restriction"`
-}
-
-type ComplexContent struct {
-	XMLName     xml.Name    `xml:"complexContent"`
-	Extension   Extension   `xml:"extension"`
-	Restriction Restriction `xml:"restriction"`
-}
-
-type Extension struct {
-	XMLName    xml.Name    `xml:"extension"`
-	Base       string      `xml:"base,attr"`
-	Sequence   Sequence    `xml:"sequence"`
-	Choice     Choice      `xml:"choice"`
-	Attributes []Attribute `xml:"attribute"`
-}
-
-type Sequence struct {
-	XMLName      xml.Name      `xml:"sequence"`
-	ComplexTypes []ComplexType `xml:"complexType"`
-	Elements     []Element     `xml:"element"`
-	Any          []AnyElement  `xml:"any"`
-	Choices      []Choice      `xml:"choice"`
-}
-
-type Choice struct {
-	XMLName      xml.Name      `xml:"choice"`
-	ComplexTypes []ComplexType `xml:"complexType"`
-	Elements     []Element     `xml:"element"`
-	Any          []AnyElement  `xml:"any"`
-}
-
-type Attribute struct {
-	XMLName   xml.Name `xml:"attribute"`
-	Name      string   `xml:"name,attr"`
-	Ref       string   `xml:"ref,attr"`
-	Type      string   `xml:"type,attr"`
-	ArrayType string   `xml:"arrayType,attr"`
-	Min       int      `xml:"minOccurs,attr"`
-	Max       string   `xml:"maxOccurs,attr"`
-	Nillable  bool     `xml:"nillable,attr"`
-}
-
-type Element struct {
-	XMLName     xml.Name    `xml:"element"`
-	Name        string      `xml:"name,attr"`
-	Ref         string      `xml:"ref,attr"`
-	Type        string      `xml:"type,attr"`
-	Min         int         `xml:"minOccurs,attr"`
-	Max         string      `xml:"maxOccurs,attr"`
-	Nillable    bool        `xml:"nillable,attr"`
-	ComplexType ComplexType `xml:"complexType"`
-}
-
-type AnyElement struct {
-	XMLName xml.Name `xml:"any"`
-	Min     int      `xml:"minOccurs,attr"`
-	Max     string   `xml:"maxOccurs,attr"`
-}
-
-type ImportSchema struct {
-	XMLName   xml.Name `xml:"import"`
-	Namespace string   `xml:"namespace,attr"`
-	Location  string   `xml:"schemaLocation,attr"`
-}
-
-type IncludeSchema struct {
-	XMLName   xml.Name `xml:"include"`
-	Namespace string   `xml:"namespace,attr"`
-	Location  string   `xml:"schemaLocation,attr"`
-}
-
 func main() {
-	fileName := "./wsdl-xsd/hcs-house-management-types.xsd"
+	xsdFile := "./wsdl-xsd/hcs-rvuo-types.xsd"
+	xsdData := getFileData(xsdFile)
+	xsdSchema := getSchema(xsdData)
+
+	wsdlFile := "./wsdl-xsd/hcs-rvuo-service-async.wsdl"
+	wsdlData := getFileData(wsdlFile)
+	wsdlDef := getDefinition(wsdlData)
+	fmt.Println(xsdSchema, wsdlDef)
+}
+
+func getFileData(fileName string) []byte {
 	data, err := os.ReadFile(fileName)
 	if err != nil {
 		panic(err)
 	}
+	return data
+}
+
+func getSchema(data []byte) Schema {
 	var schema Schema
-	err = xml.Unmarshal(data, &schema)
+	err := xml.Unmarshal(data, &schema)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(schema)
+	return schema
+}
+
+func getDefinition(data []byte) Definitions {
+	var def Definitions
+	err := xml.Unmarshal(data, &def)
+	if err != nil {
+		panic(err)
+	}
+	return def
 }
